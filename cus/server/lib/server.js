d59c9bdafc6aa4ebae8733a7824197a888c944b6
langPu <2439026868@qq.com>
2015-12-15 11:42:33 +0800

    个人主页调试

---
+++ cus/server/lib/server.js
@@ -0,0 +1,38 @@
+/**
+ * @description
+ */
+var md5 = require('md5');
+var _ = require('lodash');
+module.exports = {
+	//把数据转换成后台需要的数据格式
+	parserData: function (data, service) {
+		var sign = '1234567890!@#$%^&*';
+		data = _.mapKeys(data, function (v, k) {
+			return k.toLowerCase();
+		});
+		var dataString = JSON.stringify(data);
+		var temp = {
+			partnerId: 'bdbvip',
+			time: '0',
+			version: '1.0',
+			service: service
+		};
+		//生成加密串
+		var key = md5(dataString + temp.partnerId +
+				service + temp.time + temp.version + sign);
+		//将所有的key转成小写
+		temp = _.mapKeys(temp, function (v, k) {
+			return k.toLowerCase();
+		});
+		var string = 'data=' + dataString;
+		temp.key = key;
+		_.map(temp, function (v, k) {
+			string += '&' + k + '=' + v;
+		});
+		return string;
+	},
+	//获取serverId
+	getServerId: function () {
+		return 'BDB';
+	}
+};

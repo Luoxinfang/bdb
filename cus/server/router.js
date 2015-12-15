7cee66bab418b91c37befa748a1eaf528b311216
langPu <2439026868@qq.com>
2015-12-15 14:08:12 +0800

    修改请求的路由的路径

--- cus/server/router.js
+++ cus/server/router.js
@@ -39,68 +39,68 @@ module.exports = function (router) {
 		res.render('cus/page/user/login.tpl', obj);
 	});
 	//注册 —— 输入电话号码
-	router.get('/reg-tel', function (req, res, next) {
+	router.get('/user/reg-tel', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '注册';
 		obj.header.leftUrl = '/login';
 		res.render('cus/page/user/reg-tel.tpl', obj);
 	});
 	//注册 —— 设置密码
-	router.get('/reg-pwd', function (req, res, next) {
+	router.get('/user/reg-pwd', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '注册';
 		obj.header.leftUrl = '/reg-tel';
 		res.render('cus/page/user/reg-pwd.tpl', obj);
 	});
 	//找回密码 —— 输入信息
-	router.get('/find-pwd-info', function (req, res, next) {
+	router.get('/user/find-pwd-info', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '找回密码';
 		obj.header.leftUrl = '/login';
 		res.render('cus/page/user/find-pwd-info.tpl', obj);
 	});
 	//找回密码 —— 设置新密码
-	router.get('/find-pwd-new', function (req, res, next) {
+	router.get('/user/find-pwd-new', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '找回密码';
 		obj.header.leftUrl = '/find-pwd-info';
 		res.render('cus/page/user/find-pwd-new.tpl', obj);
 	});
 	//找回密码 —— 找回密码结果页
-	router.get('/find-pwd-result', function (req, res, next) {
+	router.get('/user/find-pwd-result', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '找回密码';
 		obj.header.leftIcon = false;
 		res.render('cus/page/user/find-pwd-result.tpl', obj);
 	});
 	//绑定第三方账号 —— 输入信息
-	router.get('/bind-account-info', function (req, res, next) {
+	router.get('/user/bind-account-info', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '绑定第三方账号';
 		obj.header.leftIcon = false;
 		res.render('cus/page/user/bind-account-info.tpl', obj);
 	});
 	//绑定第三方账号 —— 设置新密码
-	router.get('/bind-account-pwd', function (req, res, next) {
+	router.get('/user/bind-account-pwd', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '绑定第三方账号';
 		obj.header.leftUrl = '/bind-account-info';
 		res.render('cus/page/user/bind-account-pwd.tpl', obj);
 	});
 	//修改密码
-	router.get('/change-pwd', function (req, res, next) {
+	router.get('/user/change-pwd', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '修改密码';
 		res.render('cus/page/user/change-pwd.tpl', obj);
 	});
 	//更换手机号 —— 验证
-	router.get('/change-tel-info', function (req, res, next) {
+	router.get('/user/change-tel-info', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '更换手机号';
 		res.render('cus/page/user/change-tel-info.tpl', obj);
 	});
 	//更换手机号 —— 输入新号码
-	router.get('/change-tel-new', function (req, res, next) {
+	router.get('/user/change-tel-new', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '更换手机号';
 		obj.header.leftUrl = '/change-tel-info';
@@ -114,7 +114,7 @@ module.exports = function (router) {
 		res.render('cus/page/user/personal-card.tpl', obj);
 	});
 	//收货地址
-	router.get('/receipt-address', function (req, res, next) {
+	router.get('/user/receipt-address', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '收货地址';
 		obj.header.leftUrl = '/user/personal-card';
@@ -134,28 +134,28 @@ module.exports = function (router) {
 		res.render('cus/page/wallet/trans-list.tpl', obj);
 	});
 	//我的钱包 —— 提现
-	router.get('/withdrawals', function (req, res, next) {
+	router.get('/wallet/withdrawals', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '提现';
 		obj.header.leftUrl = '/wallet';
 		res.render('cus/page/wallet/withdrawals.tpl', obj);
 	});
 	//我的钱包 —— 充值
-	router.get('/recharges', function (req, res, next) {
+	router.get('/wallet/recharges', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '充值';
 		obj.header.leftUrl = '/wallet';
 		res.render('cus/page/wallet/recharges.tpl', obj);
 	});
 	//我的钱包 —— 提现结果页
-	router.get('/withdrawals-result', function (req, res, next) {
+	router.get('/wallet/withdrawals-result', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '提现';
 		obj.header.leftUrl = '/wallet';
 		res.render('cus/page/wallet/withdrawals-result.tpl', obj);
 	});
 	//我的钱包 ——添加银行卡
-	router.get('/add-bank-card', function (req, res, next) {
+	router.get('/wallet/add-bank-card', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '添加银行卡';
 		obj.header.leftUrl = '/wallet';
@@ -245,68 +245,68 @@ module.exports = function (router) {
 		res.render('cus/page/weui.tpl', obj);
 	});
 	//系统消息
-	router.get('/sys-message', function (req, res, next) {
+	router.get('/user/sys-message', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		res.render('cus/page/user/sys-message.tpl', obj);
 	});
 	//用户消息
-	router.get('/user-message', function (req, res, next) {
+	router.get('/user/user-message', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		res.render('cus/page/user/user-message.tpl', obj);
 	});
 	//卡片管理
-	router.get('/card-management', function (req, res, next) {
+	router.get('/user/card-management', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '银行卡管理';
 		obj.header.rightText = '删除';
 		res.render('cus/page/user/card-management.tpl', obj);
 	});
 	//系统设置
-	router.get('/sys-settings', function (req, res, next) {
+	router.get('/settings/sys-settings', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '系统设置';
 		obj.header.set = ['安全', '推送设置', '更新版本', '关于百多宝', '投诉建议']
 		res.render('cus/page/settings/sys-settings.tpl', obj);
 	});
 	//安全设置
-	router.get('/safety-settings', function (req, res, next) {
+	router.get('/user/safety-settings', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '系统设置';
 		obj.header.set = ['修改密码', '更换手机号']
 		res.render('cus/page/user/sys-settings.tpl', obj);
 	});
 	//推送设置
-	router.get('/push-settings', function (req, res, next) {
+	router.get('/settings/push-settings', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '推送设置';
 		res.render('cus/page/settings/push-settings.tpl', obj);
 	});
 	//版本更新
-	router.get('/version-update', function (req, res, next) {
+	router.get('/settings/version-update', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '版本更新';
 		res.render('cus/page/settings/version-update.tpl', obj);
 	});
 	//关于百多宝
-	router.get('/about-bdb', function (req, res, next) {
+	router.get('/settings/about-bdb', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '关于百多宝';
 		res.render('cus/page/settings/about-bdb.tpl', obj);
 	});
 	//投诉建议
-	router.get('/suggestions', function (req, res, next) {
+	router.get('/settings/suggestions', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '投诉建议';
 		res.render('cus/page/settings/suggestions.tpl', obj);
 	});
 	//退款
-	router.get('/to-return', function (req, res, next) {
+	router.get('/user/to-return', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '退款';
 		res.render('cus/page/user/to-return.tpl', obj);
 	});
 	//申请平台介入
-	router.get('/application-bdb', function (req, res, next) {
+	router.get('/user/application-bdb', function (req, res, next) {
 		var obj = _.cloneDeep(resObj);
 		obj.header.title = '申请平台介入';
 		res.render('cus/page/user/application-bdb.tpl', obj);

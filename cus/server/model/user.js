8e83ebbc6f7be77a1c707985609ddecd041b2fb4
xf.radish <xf.radish@gmail.com>
2015-12-16 10:59:26 +0800

    提交代码

--- cus/server/model/user.js
+++ cus/server/model/user.js
@@ -5,9 +5,7 @@ var serverId = server.getServerId();
 var serviceName = 'user'
 module.exports = {
   getData: function (data) {
-    console.log(data);
     var query = server.parserData(data, serviceName);
-    //console.log('/interface/user/index?' + query);
     return yog.ral(serverId, {
       path: '/interface/user/index?' + query
     });

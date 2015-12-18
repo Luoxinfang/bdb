31a34cb1ddd1599e76fdd96a3ab6f869ba51be68
langPu <2439026868@qq.com>
2015-12-18 19:10:00 +0800

    完善关注店铺的接口

--- cus/client/widget/store/store.js
+++ cus/client/widget/store/store.js
@@ -1,38 +1,40 @@
 /**
- * @description pulang 2015/12/16
+ * @author pulang
+ * @createTime 2015-12-18
+ * @description 这个路由处理地址信息的请求
  */
 
-var _ = require('lodash');
-var model = yog.require('cus/model/user.js');
+var B = require('_common:js/bdb/core.js');
 
-module.exports = function (req, res, next) {
-	var resObj = req.appData;
-	var page = {
-		Page:1,
-		Pagesize:2
-	};
-	var params1 = {
-		token: req.session.user.token
-	};
-	var params2 = _.extend({
-		token: req.session.user.token
-	},page);
-	//获取用户基本信息
-	model.getData(params1).then(function (rs) {
-		resObj.userInfo = rs.data;
-		if(null != rs.data.othercount){
-			resObj.userInfo.counttype = rs.data.othercount.substring(0,2);
-		}
-		//查询收货地址列表信息
-		model.queryList(params2).then(function (rs) {
-			resObj.addressInfo = rs.list;
-			res.render('cus/page/user/personal.tpl',resObj);
-		}).catch(function (error) {
-			console.log(error);
-			yog.log.fatal(error);
+module.exports = {
+	init: function () {
+		this.event();
+	},
+	//修改用户个人信息
+	attention: function () {
+
+		$.ajax({
+			type: 'post',
+			dataType: 'json',
+			url: '/_common/store/attention',
+			data: {
+				touserid: 1,
+				attentiontype:1
+			},
+			success: function (data) {
+				if (0 == data.status) {
+					/*console.log('asd');*/
+				} else {
+					var msg = data.msg || '服务器异常，请稍后再试';
+					B.topWarn(msg);
+				}
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				B.topWarn('服务器异常，请稍后再试');
+			}
 		});
-	}).catch(function (error) {
-		console.log(error);
-		yog.log.fatal(error);
-	});
+	},
+	event: function () {
+		$(".header").on('click', "#collect", this.attention.bind(this));
+	}
 };

aae8d19968dbc829a61bb1f9801f395f8972ff52
xf.radish <xf.radish@gmail.com>
2015-12-25 19:40:09 +0800

    添加活动的js

---
+++ cus/server/action/auction/activity.js
@@ -0,0 +1,22 @@
+/**
+ * @description
+ */
+
+var _ = require('lodash');
+var model = yog.require('cus/model/auction/detail.js');
+
+module.exports.get = function (req, res) {
+  var param = {
+    proCode: req.params.id,
+    token: req.session.user.token
+  };
+  model.getDetail(param).then(function (rs) {
+    var resObj = req.appData;
+    resObj.data = rs.data;
+    resObj.data.id = param.proCode;//回传id
+    resObj.header.title = rs.data.proname;
+    resObj.header.rightIcons = [{icon: 'collect'}, {icon: 'share'}];
+    resObj.auctionStatus = req.query.status == null ? 0 : req.query.status;
+    res.render('cus/page/auction/detail.tpl', resObj);
+  });
+};

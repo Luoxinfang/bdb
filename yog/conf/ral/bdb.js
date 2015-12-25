a17221ddce0bc33161eb383c73cc34b37459eb1b
chenzhenhua <935370724@qq.com>
2015-12-25 17:23:52 +0800

    Merge branch 'master' of E:\WebstormProjects\bdb with conflicts.

--- yog/conf/ral/bdb.js
+++ yog/conf/ral/bdb.js
@@ -3,19 +3,20 @@
  * @author radish
  */
 module.exports = {
-  'BDB': {
-    unpack: 'json',
-    protocol: 'http',
-    pack: 'querystring',
-    method: 'POST',
-    encoding: 'utf-8',
-    balance: 'random',
-    retry: 0,
-    timeout: 500,
-    server: [
-      //{host: '192.168.0.131', port: 8080}, //王勇
-      //{host: '192.168.0.105', port: 8080}, //少飞
-      {host: '192.168.0.110', port: 8080}
-    ]
-  }
+	'BDB': {
+		unpack: 'json',
+		protocol: 'http',
+		pack: 'querystring',
+		method: 'POST',
+		encoding: 'utf-8',
+		balance: 'random',
+		retry: 0,
+		timeout: 500,
+		server: [
+			//{host: '192.168.0.105', port: 8080}
+			//{host: '192.168.0.133', port: 8080}  //王天文
+			//{host: '192.168.0.131', port: 8080}  //王勇
+			{host: 'flying', port: 8080}         //周少飞
+		]
+	}
 };
